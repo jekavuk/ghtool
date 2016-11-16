3855a6525d3241c9e1bc08e31b2ee15ae3aeaca1
Jelena <jekavuk@gmail.com>
2016-11-16 10:34:53 +0100

    fixed sorting

--- src/main/java/ghtool/processor/GHToolProcessorHelper.java
+++ src/main/java/ghtool/processor/GHToolProcessorHelper.java
@@ -9,7 +9,7 @@ public class GHToolProcessorHelper {
 	private static final String HOST					= "api.github.com";
 	private static final String SEARCH_REPOSITORIES		= "/search/repositories";
 	private static final String TYPE_REPOSITORIES		= "type=Repositories";
-	private static final String DEFAULT_SORT			= "s=updated";
+	private static final String DEFAULT_SORT			= "sort=updated";
 	private static final String DEFAULT_SORT_ORDER		= "desc";
 	private static final String LANGUAGE 				= "language";
 	
@@ -40,13 +40,6 @@ public class GHToolProcessorHelper {
 			url.append(PER_PAGE + "=" + DEFAULT_NUMBER_OF_RESULTS);
 		}
 		url.append("&" + PAGE + "=" + pageNum);
-		if(currentCommandsMap.containsKey(GHToolComandsParser.SORT_BY_DATE_UPDATED)) {
-			if (!currentCommandsMap.get(GHToolComandsParser.SORT_BY_DATE_UPDATED).isEmpty()) {
-				url.append("&o=" + currentCommandsMap.get(GHToolComandsParser.SORT_BY_DATE_UPDATED));
-			} else {
-				url.append("&o=" + DEFAULT_SORT_ORDER);
-			}
-		}
 		url.append("&q=created%3A>1970-01-01");
 		if(currentCommandsMap.containsKey(GHToolComandsParser.DESC)) {
 			url.append("+" + currentCommandsMap.get(GHToolComandsParser.DESC));
@@ -56,6 +49,13 @@ public class GHToolProcessorHelper {
 		}
 		url.append("&" + TYPE_REPOSITORIES);
 		url.append("&" + DEFAULT_SORT);
+		if(currentCommandsMap.containsKey(GHToolComandsParser.SORT_BY_DATE_UPDATED)) {
+			if (!currentCommandsMap.get(GHToolComandsParser.SORT_BY_DATE_UPDATED).isEmpty()) {
+				url.append("&order=" + currentCommandsMap.get(GHToolComandsParser.SORT_BY_DATE_UPDATED));
+			} else {
+				url.append("&order=" + DEFAULT_SORT_ORDER);
+			}
+		}
 		
 		return url.toString();
 		//return "https://api.github.com/search/repositories?per_page=25&q=language%3Ajava&type=Repositories";
